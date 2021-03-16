c6b9e375ede864dbfcdc1eb3862ad150b1e52198
huluBrother <xiangzi535@126.com>
2021-03-16 15:18:08 +0800

    添加java的项目用来编写算法题

---
+++ algorithms/src/main/java/org/leetcode/Application.java
@@ -0,0 +1,13 @@
+package org.leetcode;
+
+/**
+ * @ProjectName: algorithmsJava
+ * @Package: org.leetcode
+ * @ClassName: Application
+ * @Author: zhanghx
+ * @Description:
+ * @Date: 2021/3/16 15:17
+ * @Version: 1.0
+ */
+public class Application {
+}
